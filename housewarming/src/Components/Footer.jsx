05f4b8ece54d2f05db5054994430166d228110bf
Dilli_Rao <101472151+legendarycopyninja@users.noreply.github.com>
2023-02-05 01:37:21 +0530

    Second Commit

--- housewarming/src/Components/Footer.jsx
+++ housewarming/src/Components/Footer.jsx
@@ -1,7 +1,27 @@
 export default function Footer(){
     return (
         <footer>
-            
+        <h3>Contacts</h3>
+        <div className="linkContainer">
+        <a href="tel:+918870142198" className="phonelinks">
+                <i class='fas fa-phone'></i>
+        <p>Kalyan</p>
+        </a>
+        <a href="tel:+919739655454" className="phonelinks">
+                <i class='fas fa-phone'></i>
+        <p>Karnunakaran</p>
+        </a>
+        <a href="tel:+919566162071" className="phonelinks">
+                <i class='fas fa-phone'></i>
+        <p>Nimmi</p>
+        </a>
+        <a href="tel:+919600578546" className="phonelinks">
+                <i class='fas fa-phone'></i>
+        <p>Jeevitha</p>
+        </a>
+
+        </div>
+     
         </footer>
     )
 }
