d9dc4635d4463c72a94219e578ab38034cbad334
Dilli_Rao <101472151+legendarycopyninja@users.noreply.github.com>
2023-02-05 22:23:52 +0530

    intermediate commit

--- housewarming/src/Components/Header.jsx
+++ housewarming/src/Components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header(){
     return (
         <header className="headContainer">
             <h1>We Cordially Invite Everyone</h1>
-            <img src="logo.jpg" alt="karna Family" className="familyphoto" />
+            <img src="slogan.png" alt="house welcome"  />
             <h2>To Our HouseWarming</h2>
         </header>
     )
